6a383bccc22a8eaebfbae942e6b0343d25e7a2c4
ojinugwa <45524127+ojinugwa@users.noreply.github.com>
2023-06-08 17:50:34 -0400

    Fix bug on SIgn up page

--- innoscripta-blog-front-end/src/Pages/SignUp/SignUp.js
+++ innoscripta-blog-front-end/src/Pages/SignUp/SignUp.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import InputField from '../../Components/Form/InputField';
-import FormLayout from '../../Components/Form/FOrmLayout';
+import FormLayout from '../../Components/Form/FormLayout';
 
 function SingUp({ }) {
 
